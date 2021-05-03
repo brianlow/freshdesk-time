a58bf614c0327c929cbb36409e408ab1bb11133f
Brian Low <brian.low@shift.com>
2021-05-03 03:28:37 -0600

    Working with Google API + OAuth

--- time.rb
+++ time.rb
@@ -9,6 +9,7 @@ gemfile do
   gem 'pry'
   gem 'rainbow'
   gem 'tty-table'
+  gem 'google-api-client'
 end
 
 require 'json'
@@ -21,11 +22,60 @@ require 'csv'
 require 'ostruct'
 require 'pry'
 require 'active_support/all'
+require 'google/apis/sheets_v4'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
+
 require_relative 'timing_csv'
 require_relative 'timesheet'
 
 filename = ARGV[0]
 
-entries = TimingCsv.new.parse(filename)
+APPLICATION_NAME = 'Google Sheets API Ruby Quickstart'.freeze
+CREDENTIALS_PATH = 'credentials.json'.freeze
 
-Timesheet.new(entries).print
+# scope = 'https://www.googleapis.com/auth/sheets'
+# authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+#   json_key_io: File.open(CREDENTIALS_PATH),
+#   scope: scope
+# )
+# authorizer.fetch_access_token!
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
+# The file token.yaml stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+TOKEN_PATH = 'token.yaml'.freeze
+SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
+  token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
+  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
+  user_id = 'default'
+  credentials = authorizer.get_credentials user_id
+  if credentials.nil?
+    url = authorizer.get_authorization_url base_url: OOB_URI
+    puts 'Open the following URL in the browser and enter the ' \
+         "resulting code after authorization:\n" + url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI
+    )
+  end
+  credentials
+end
+
+service = Google::Apis::SheetsV4::SheetsService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+
+# entries = TimingCsv.new.parse(filename)
+
+# Timesheet.new(entries).print
