0ebe3ef5d1de19b27d2077a4c8b58160407761de
Brian Low <brian.low@shift.com>
2021-05-31 17:53:52 -0600

    Client for duplicate and setting cells

---
+++ google_spreadsheet.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class GoogleSpreadsheet
+  APPLICATION_NAME = 'Ruby Timesheet'
+  CREDENTIALS_PATH = 'service-credentials.json'
+  SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+
+  def initialize(spreadsheet_id)
+    @spreadsheet_id = spreadsheet_id
+  end
+
+  def sheet_exists?(sheet_name)
+    spreadsheet.sheets.any? { |s| s.properties.title == sheet_name }
+  end
+
+  def duplicate_sheet(source_sheet, target_sheet)
+    template = spreadsheet.sheets.find { |s| s.properties.title == source_sheet }
+
+    duplicate = Google::Apis::SheetsV4::DuplicateSheetRequest.new(
+      source_sheet_id: template.properties.sheet_id,
+      new_sheet_name: target_sheet,
+      insert_sheet_index: 1
+    )
+    request = Google::Apis::SheetsV4::Request.new(duplicate_sheet: duplicate)
+    batch = Google::Apis::SheetsV4::BatchUpdateSpreadsheetRequest.new(requests: [request])
+    service.batch_update_spreadsheet(@spreadsheet_id, batch)
+  end
+
+  def set_cell(sheet_name, cell, value)
+    service.update_spreadsheet_value(
+      @spreadsheet_id,
+      "'#{sheet_name}'!#{cell}",
+      Google::Apis::SheetsV4::ValueRange.new(values: [[value]]),
+      value_input_option: 'RAW'
+    )
+  end
+
+  private
+
+  def spreadsheet
+    @spreadsheet ||= service.get_spreadsheet(@spreadsheet_id)
+  end
+
+  def service
+    @service ||= build_service
+  end
+
+  def build_service
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.client_options.application_name = APPLICATION_NAME
+    service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(CREDENTIALS_PATH),
+      scope: SCOPE
+    )
+    service.authorization.fetch_access_token!
+    service
+  end
+end
--- time.rb
+++ time.rb
@@ -25,12 +25,15 @@ require 'pry'
 require 'active_support/all'
 require 'google/apis/sheets_v4'
 require 'googleauth'
-require 'googleauth/stores/file_token_store'
 require 'fileutils'
 
-require_relative 'goog'
+require_relative 'google_spreadsheet'
 require_relative 'timing_csv'
 require_relative 'timesheet'
+
+def round(hours)
+  (hours * 2.0).round / 2.0
+end
 
 spreadsheet_id = '17Z7EGb2AFZiMRL2JC0abEdrCY1klXod2tWWTM98q3j8' # Temp Spreadsheet
 csv_folder = '/Users/brianshift/Downloads/time'
@@ -38,36 +41,22 @@ csv_folder = '/Users/brianshift/Downloads/time'
 month = Date.new(2021, 5, 1)
 
 csv = TimingCsv.new.parse("#{csv_folder}/#{month.strftime('%Y-%m')}.csv")
-pp csv
+hours_by_date = csv.map { |row| [row.date, round(row.hours)] }.to_h
 
-service = Goog.sheets
 sheet_name = month.strftime('%b %Y')
-spreadsheet = service.get_spreadsheet(spreadsheet_id)
-sheet = spreadsheet.sheets.find { |s| s.properties.title == sheet_name }
-
-if sheet.blank?
-  template = spreadsheet.sheets.find { |s| s.properties.title == 'Template' }
-
-  duplicate = Google::Apis::SheetsV4::DuplicateSheetRequest.new(
-    source_sheet_id: template.properties.sheet_id,
-    new_sheet_name: sheet_name,
-    insert_sheet_index: 1
-  )
-  request = Google::Apis::SheetsV4::Request.new(duplicate_sheet: duplicate)
-  batch = Google::Apis::SheetsV4::BatchUpdateSpreadsheetRequest.new(requests: [request])
-  service.batch_update_spreadsheet(spreadsheet_id, batch)
-
-  sheet = spreadsheet.sheets.find { |s| s.properties.title == sheet_name }
-end
+spreadsheet = GoogleSpreadsheet.new(spreadsheet_id)
+spreadsheet.duplicate_sheet('Template', sheet_name) unless spreadsheet.sheet_exists?(sheet_name)
+spreadsheet.set_cell(sheet_name, 'B6', 'Hey')
 
 # https://github.com/googleapis/google-api-ruby-client/tree/master/google-api-client/generated/google/apis/sheets_v4
 # https://stackoverflow.com/questions/59231487/google-sheets-api-v4-bold-part-of-cell
 # https://developers.google.com/sheets/api/samples/formatting
 
-binding.pry
-i = 1
 # Find sheet
 
 # entries = TimingCsv.new.parse(filename)
 
 # Timesheet.new(entries).print
+
+binding.pry
+i = 0
